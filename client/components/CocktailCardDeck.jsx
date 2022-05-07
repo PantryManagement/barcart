cabfc7351d6f446adae7a64140c9d34866b04e56
Joseph Park <joeepark@gmail.com>
2022-05-07 10:50:31 -0700

    Build structure components and frontpage rendering

---
+++ client/components/CocktailCardDeck.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CocktailCard from './CocktailCard.jsx';
+
+export default function CocktailCardDeck() {
+  return (
+    <div className='cocktail-card-deck'>
+      <CocktailCard />
+    </div>
+  )
+}   
