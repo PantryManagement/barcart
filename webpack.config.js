9d8d3bb995235e2e431eac8f1d146db00792a631
cartersamj <sammahcarter@gmail.com>
2022-05-06 19:02:56 -0700

    built boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: path.resolve('./client/index.js'),
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'build.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'dev server app',
+      template: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: 'index.html',
+    },
+    port: 8080,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx|\.js/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css|\.scss|\.sass/gi,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+
