77d16362f0da6baf72a07f399cf732b3cf837e20
Christopher LeBrett <christopherlebrett@Christophers-MacBook-Pro.local>
2022-05-09 16:36:43 -0700

    serves drinks user can make based on their inventory

--- server/controllers/drinksController.js
+++ server/controllers/drinksController.js
@@ -3,7 +3,36 @@ const drinksController = {};
 
 drinksController.getDrinks = (req, res, next) => {
 
-  
+  // query the database for the ingredients_in_drinks table with a column added for "ingredient" name, "in_stock" amount and for "drink" name
+
+  const queryStr = 'SELECT ingredients_in_drinks._id, ingredients_in_drinks.drink_id, ingredients_in_drinks.ingredient_id, ingredients_in_drinks.quantity, ingredients.ingredient, ingredients.in_stock, drinks.drink FROM ingredients_in_drinks LEFT JOIN ingredients ON ingredients_in_drinks.ingredient_id=ingredients._id LEFT JOIN drinks ON ingredients_in_drinks.drink_id=drinks._id';
+
+  db.query(queryStr)
+  .then((data) => {
+    let results = data.rows;
+    let possible = {};
+
+    for (let el of results) {
+      let numDrinks = Math.floor(el.in_stock / el.quantity);
+      let drinkName = el.drink;
+      
+      if (possible.hasOwnProperty(drinkName)) { 
+        if (numDrinks < possible[drinkName]) {
+          possible[drinkName] = numDrinks;
+        }
+      } else {
+        possible[drinkName] = numDrinks;
+      }
+    }
+
+    res.locals.drinks = possible;
+    return next();
+
+  })
+  .catch(error => next({
+    log: 'drinksController.getDrinks ERROR',
+    message: {err: 'drinksController.getDrinks ERROR'}
+  }));
 
 };
 
